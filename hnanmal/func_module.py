965b124cf8ed65916858aa4b1c29b0e781e98dcb
pjmk007 <pjmk007@gmail.com>
2022-03-23 10:59:16 +0900

    adsfaasd

---
+++ hnanmal/func_module.py
@@ -0,0 +1,27 @@
+
+def func0():
+    return "init_value"
+
+def func1(x):
+    return pow(x,x)
+
+def func2(x):
+    return x % 1111
+
+def func3(x):
+    return x % 109 + 85
+
+def func4(x):
+    return x % 214 - 37
+
+def func5(x):
+    return (x % 325) * 3 + 31
+
+def func6(x):
+    return x % 777 -126
+
+def func7():
+    pass
+
+def func8():
+    pass
